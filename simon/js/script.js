ae8903d51e4056bd1595b4a1e7bd20aa9a27a7e8
Celeste Glavin <cmugla@gmail.com>
2016-06-10 14:57:30 -0400

    add click events for mobile use

--- simon/js/script.js
+++ simon/js/script.js
@@ -59,10 +59,34 @@ $(document).ready(function(){
 
   function iterateUser() {
     // listen for which key is pressed
-    $(document).off("keydown", userPlay).on("keydown", userPlay);
+    $(document).off("keydown", checkKeyPress).on("keydown", checkKeyPress);
+
+    // click for mobile
+    $('#blue').off('click',checkClick).click(checkClick);
+    $('#red').off('click',checkClick).click(checkClick);
+    $('#yellow').off('click',checkClick).click(checkClick);
+    $('#green').off('click',checkClick).click(checkClick);
   }
 
-  function userPlay(event) {
+  function checkClick() {
+    if (this===$('#blue')[0]) {
+      userInput('#blue');
+      console.log('blue');
+    } else if (this===$('#red')[0]) {
+      userInput('#red');
+      console.log('red');
+    } else if (this===$('#yellow')[0]) {
+      userInput('#yellow');
+      console.log('yelow');
+    } else if (this===$('#green')[0]) {
+      userInput('#green');
+      console.log('green');
+    } else {
+      console.log(this);
+    }
+  }
+
+  function checkKeyPress(event) {
     // remove any lost classes
     $('div').removeClass();
     // depending on which key pressed, show an animation for the appropriate div
